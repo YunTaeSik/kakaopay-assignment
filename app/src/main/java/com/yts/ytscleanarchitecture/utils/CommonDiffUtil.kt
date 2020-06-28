326d73284c2cea3b64ac9acd20003d4a63881180
YunTaeSik <sky877kr@gmail.com>
2020-06-28 18:51:33 +0900

    1. DiffUtil 설명 추가

--- app/src/main/java/com/yts/ytscleanarchitecture/utils/CommonDiffUtil.kt
+++ app/src/main/java/com/yts/ytscleanarchitecture/utils/CommonDiffUtil.kt
@@ -4,10 +4,10 @@ import androidx.recyclerview.widget.DiffUtil
 import com.google.gson.Gson
 
 class CommonDiffUtil<B> : DiffUtil.ItemCallback<B>() {
-    override fun areItemsTheSame(oldItem: B, newItem: B): Boolean {
+    override fun areItemsTheSame(oldItem: B, newItem: B): Boolean { //객체가 같은지 여부
         return oldItem == newItem
     }
-    override fun areContentsTheSame(oldItem: B, newItem: B): Boolean {
+    override fun areContentsTheSame(oldItem: B, newItem: B): Boolean { //객체 안에 내용물이 같은지에 대한 여부
         return (Gson().toJson(oldItem)) == (Gson().toJson(newItem))
     }
 
